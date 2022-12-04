6b1233f2cc38944bcbb7431cfec0cbac63841d6d
lihuen <lcarranza@fi.uba.ar>
2022-12-04 18:20:26 -0300

    probando cosas

--- src/main/java/org/example/juego/App.java
+++ src/main/java/org/example/juego/App.java
@@ -1,19 +1,35 @@
 package org.example.juego;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        var label = new Label("Hello que tal");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
+        stage.setScene(new Scene(createContent()));
         stage.show();
+    }
+
+    private Parent createContent() {
+        TextField input = new TextField();
+        Text output = new Text();
+        Button button = new Button("Press");
+        button.setOnAction(e -> {
+            output.setText(input.getText());
+        });
+        TilePane root = new TilePane(input, button, output);
+        root.setPrefSize(300,300);
+        return root;
     }
 
     public static void main(String[] args){
