b5cd46043a45194e71f3095f2f77a3f66a018551
lihuen <lcarranza@fi.uba.ar>
2022-12-07 16:49:08 -0300

    lista de flagboxes

--- src/main/java/org/example/juego/BoxFactory.java
+++ src/main/java/org/example/juego/BoxFactory.java
@@ -5,10 +5,11 @@ import org.example.juego.Boxes.ClickedBox;
 import org.example.juego.Boxes.BombBox;
 
 public class BoxFactory {
-    public static enum Type { BOMB, BOX, CLICK };
-    private static final BombBox bombBox = new BombBox();
-    private static org.example.juego.Boxes.Box[] Box = new Box[11];
+    public static enum Type { BOMB, BOX, CLICK, FLAG };
+    private static final BombBox bombBox = new BombBox(10);
+    private static org.example.juego.Boxes.Box[] box = new Box[11];
     private static final Box[] clickedBox = new ClickedBox[11];
+    private static final Box[] flagBox = new Box[11];
 
     public static Box createBox(Type type, int number) {
 
@@ -16,13 +17,20 @@ public class BoxFactory {
             case BOMB:
                 return bombBox;
             case BOX:
-                if (Box[number] == null)
-                    Box[number] = new Box(number);
-                return Box[number];
+                if (box[number] == null)
+                    box[number] = new Box(number);
+                return box[number];
             case CLICK:
                 if (clickedBox[number] == null)
                     clickedBox[number] = new ClickedBox(number);
                 return clickedBox[number];
+            case FLAG:
+                if (flagBox[number] == null){
+                    flagBox[number] = new Box(number);
+                    flagBox[number].setFlag();
+                }
+                return flagBox[number];
+
         }
 
         return null;
