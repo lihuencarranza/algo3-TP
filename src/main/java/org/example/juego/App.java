1eb9cecbfdc460239e53a4d18ef3626de2d224f9
lihuen <lcarranza@fi.uba.ar>
2022-12-07 00:49:35 -0300

    cambio de estructura

--- src/main/java/org/example/juego/App.java
+++ src/main/java/org/example/juego/App.java
@@ -5,12 +5,13 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
+    private Game game;
     @Override
     public void start(Stage stage) throws Exception {
         Model model = new Model();
-        View view = new View(stage, model);
-        Controller controller = new Controller(model, view);
-        controller.start();
+        Controller controller = new Controller(model);
+        View view = new View(stage, model, controller);
+
     }
     public static void main(String[] args){
         launch();
