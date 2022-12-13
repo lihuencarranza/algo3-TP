6da2c95d6fb3ba00c5a8b5f33b55cfd31eab69c2
lihuen <lcarranza@fi.uba.ar>
2022-12-13 11:32:27 -0300

    cambio funciones de crear boxes

--- src/main/java/org/example/juego/BoxFactory.java
+++ src/main/java/org/example/juego/BoxFactory.java
@@ -3,36 +3,37 @@ package org.example.juego;
 import org.example.juego.Boxes.Box;
 import org.example.juego.Boxes.ClickedBox;
 import org.example.juego.Boxes.BombBox;
+import org.example.juego.Boxes.FlagBox;
 
 public class BoxFactory {
     public static enum Type { BOMB, BOX, CLICK, FLAG };
     private static final BombBox bombBox = new BombBox(10);
-    private static org.example.juego.Boxes.Box[] box = new Box[11];
-    private static final Box[] clickedBox = new ClickedBox[11];
-    private static final Box[] flagBox = new Box[11];
+    private static final Box[] box = new Box[11];
+    private static final ClickedBox[] clickedBox = new ClickedBox[11];
+    private static final FlagBox[] flagBox = new FlagBox[11];
 
-    public static Box createBox(Type type, int number) {
-
-        switch (type) {
-            case BOMB:
-                return bombBox;
-            case BOX:
-                if (box[number] == null)
-                    box[number] = new Box(number);
-                return box[number];
-            case CLICK:
-                if (clickedBox[number] == null)
-                    clickedBox[number] = new ClickedBox(number);
-                return clickedBox[number];
-            case FLAG:
-                if (flagBox[number] == null){
-                    flagBox[number] = new Box(number);
-                    flagBox[number].setFlag();
-                }
-                return flagBox[number];
-
-        }
-
-        return null;
+    public Box createBox(int number){
+        if (box[number] == null)
+            box[number] = new Box(number);
+        return box[number];
     }
+
+    public BombBox createBomb(){
+        return bombBox;
+    }
+
+    public ClickedBox createClickedBox(int number){
+        if (clickedBox[number] == null)
+            clickedBox[number] = new ClickedBox(number);
+        return clickedBox[number];
+    }
+
+    public FlagBox createFlagBox(int number){
+        if (flagBox[number] == null) {
+            flagBox[number] = new FlagBox(number);
+        }
+        return flagBox[number];
+    }
+
+
 }
